9a12256fb8540b4cb9196b1e197330403f26f8f1
Sanjai05122006 <sanjai2410498@ssn.edu.in>
2026-01-30 17:45:32 +0530

    Add: Unique Number of Occurrences (Java)

---
+++ leetcode/array/unique-number-of-occurrences.java
@@ -0,0 +1,26 @@
+# Unique Number of Occurrences
+
+**Problem Link:** https://leetcode.com/problems/unique-number-of-occurrences/
+
+**Language:** Java
+
+**Topics:** Array, Hash Table
+
+---
+
+```java
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+
+        // Count frequency of each number
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int value : arr) {
+            frequencyMap.put(value, frequencyMap.getOrDefault(value, 0) + 1);
+        }
+
+        // Check if all frequencies are unique
+        HashSet<Integer> frequencySet = new HashSet<>(frequencyMap.values());
+        return frequencySet.size() == frequencyMap.size();
+    }
+}
+```
